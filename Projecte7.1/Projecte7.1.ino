69fa7071d3d01a65a112700c367b01f655c08edc
marcherrera12 <marcherrera10@gmail.com>
2016-05-02 16:12:20 +0200

    Projecte7.1

---
+++ Projecte7.1/Projecte7.1.ino
@@ -0,0 +1,68 @@
+/******************************************************************************
+**   Projecte 7.1 Entrades/Sortides Analògiques.                             **
+**   Aquest programa permet reproduir paraules en morse.                     **
+**   Aquest programa requereix un montatge físic amb un altaveu.             **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+// pin for speaker
+const int pin = 8;
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //S   ** Exemple SOS **
+  punt();
+  punt();
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //O
+  ratlla();
+  ratlla();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //S
+  punt();
+  punt();
+  punt();
+  //Espai entre paraules
+  espaiP();
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
+
